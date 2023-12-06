e3ab574280ad32f37228eb50e33bcc9deb37f125
Nicholas Davis <nicholasallendavis03@gmail.com>
2023-12-05 21:46:13 -0500

    Alternatebranch so we can work side by side and deal with errors later. Finished login route. Only thing needed is connecting database!

--- models/Account.js
+++ models/Account.js
@@ -1,22 +1,42 @@
 //Creates Account Class for login in
 const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
-class Account extends Model {};
+class Account extends Model {
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
 
 Account.init(
 {
-    username:{
+    email:{
      type: DataTypes.STRING,
      allownull: false,
      primarykey: true,
+     unique: true,
+     validate:{
+        isEmail:true
+     },
  },
     password:{
      type: DataTypes.STRING,
      allownull: false,
  }
 },
-
+{
+hooks: {
+    beforeCreate: async (newUserData) => {
+      newUserData.password = await bcrypt.hash(newUserData.password, 10);
+      return newUserData;
+    },
+    beforeUpdate: async (updatedUserData) => {
+      updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+      return updatedUserData;
+    },
+},
+},
 {
     sequelize,
     timestamps: false,
--- server.js
+++ server.js
@@ -1,26 +1,34 @@
+const path = require('path');
 const express = require ('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({ });
-
-
-
-
+const routes = require('./routes');
 
 
 const app = express();
+const PORT = process.env.PORT || 3001;
 
-const routes = require('./routes');
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-app.use(session({
-    secret: 'secret',
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {
+      maxAge: 300000,
+      httpOnly: true,
+      secure: false,
+      sameSite: 'strict',
+    },
     resave: false,
-    saveUninitialized: true
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    })
+  };
+  app.use(session(sess));
 
-}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -28,6 +36,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.use(routes)
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+  });
+  
 
 
 
