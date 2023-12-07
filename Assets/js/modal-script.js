13f98ebd3b08d16e1c3ecceb5186a7ab986a9d8f
Zaxarybinx <zackscherl@gmail.com>
2023-12-06 20:05:15 -0500

    modal etc

--- Assets/js/modal-script.js
+++ Assets/js/modal-script.js
@@ -1,25 +1,47 @@
 // signup modal
-document.querySelector('#submit-sign').addEventListener('click', (event) => {
-
+document.querySelector("#submit-sign").addEventListener("click", (event) => {
   event.preventDefault();
-  
-    let userName = document.querySelector('#aligned-name').value;
-    let userPass = document.querySelector('#aligned-password').value;
-    let userMail = document.querySelector('#aligned-email').value;
-    let userNum = document.querySelector('#aligned-num').value;
-    let userDob = document.querySelector('#aligned-dob').value;
-    
-    console.log(userName, userPass, userMail, userNum, userDob);
 
-  });
+  let userName = document.querySelector("#aligned-name").value.trim();
+  let userPass = document.querySelector("#aligned-password").value.trim();
+  let userMail = document.querySelector("#aligned-email").value.trim();
+  let userNum = document.querySelector("#aligned-num").value.trim();
+  let userDob = document.querySelector("#aligned-dob").value.trim();
 
-// login modal 
-  document.querySelector('#submit-log').addEventListener('click', (event) => {
+  console.log(userName, userPass, userMail, userNum, userDob);
 
-    event.preventDefault();
-    
-      let userName = document.querySelector('#aligned-name').value;
-      let userPass = document.querySelector('#aligned-password').value;
-      
-      console.log(userName, userPass, userMail, userNum, userDob);
+  // get nick's route name for url
+  fetch("url", {
+    Method: "POST",
+    Headers: {
+      Accept: "application.json",
+      "Content-Type": "application/json",
+    },
+    Body: JSON.stringify({username: userName, userpass: userPass, userMail, userNum, userDob}),
+  })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      for (var i = 0; i < data.length; i++) {}
     });
+});
+
+// login modal
+document.querySelector("#submit-log").addEventListener("click", (event) => {
+  event.preventDefault();
+
+  let userName = document.querySelector("#aligned-name").value.trim();
+  let userPass = document.querySelector("#aligned-password").value.trim();
+
+  console.log(userName, userPass);
+
+// get nick's route name for url
+  fetch()
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      for (var i = 0; i < data.length; i++) {}
+    });
+});
